ad9b6605a4949b56592d8fc343d1a8ff06d16e39
Vinh B <vinh.bui0101@gmail.com>
2024-07-06 20:32:34 -0700

    added nexUI to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './components/**/*.{js,jsx}',
     './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
+    './node_modules/@nextui-org/**/*.{js,jsx,ts,tsx}',
   ],
   prefix: "",
   theme: {
