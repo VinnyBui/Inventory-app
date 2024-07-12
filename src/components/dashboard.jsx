197874db9fee9a7d864524bf8927df287abb0002
VinnyBui <vinh.bui0101@gmail.com>
2024-07-12 14:16:51 -0700

    Changed name and Icon

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -13,6 +13,7 @@ import {
   Package,
   Package2,
   Search,
+  ActivityIcon,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
@@ -97,8 +98,8 @@ const Dashboard = () => {
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link to="/" className="flex items-center gap-2 font-semibold">
-              <Package2 className="h-6 w-6" />
-              <span className="">Cisco Inc</span>
+              <ActivityIcon className="h-6 w-6" />
+              <span className="">WeBuyCisco</span>
             </Link>
             <div className="ml-auto h-8 w-10 flex items-center justify-center">
               <ModeToggle />
